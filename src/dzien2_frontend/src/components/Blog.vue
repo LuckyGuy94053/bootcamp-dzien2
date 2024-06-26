4dd6cf4626242d9e04822160f0b93a9897396e33
LuckyGuy94053 <nati94053@gmail.com>
2024-06-26 19:06:01 +0200

    added styles

--- src/dzien2_frontend/src/components/Blog.vue
+++ src/dzien2_frontend/src/components/Blog.vue
@@ -1,15 +1,18 @@
 <template>
     <div>
         <h2 class="text-blue-600">Wpisy na bloga:</h2>
-        <div class="w-100">
-            <button @click="pobierzWpisy">refresh</button>
+        <div class="w-100 flex flex-row-reverse">
+            <button @click="pobierzWpisy" class="bg-blue-600 rounded-sn text-white p-4">refresh</button>
         </div>
-        <div v-for="wpis in wpisy">
-            <p>{{ wpis }}</p>
+        <div class="grid mx-6 gap-4 my-4">
+            <div v-for="wpis in wpisy" class="drop-shadow-xl bg-stone-300 p-4">
+                <p>{{ wpis }}</p>
+            </div>
         </div>
-        {{ wpisy }}
-        <input v-model="nowyBlog" type="text">
-        <button @click="dodajWpisy">dodaj</button>
+        <div class="flex justify-center flex-col">
+            <input v-model="nowyBlog" type="text" class="border-2 border-blue-600 p-4">
+            <button @click="dodajWpisy" class="bg-blue-600 rounded-sn text-white p-4">dodaj</button>
+        </div>
     </div>
 </template>
 
